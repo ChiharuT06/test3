6492ab9d1ea1331bf33e25db8e80f26329eb9432
ChiharuT06 <u.chiharu0909@gmail.com>
2023-03-03 14:51:11 +0000

    4th commit

---
+++ resources/views/guests.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <title>テスト画面</title>
+    
+    <head>
+    <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body class="guest-all">
+    
+            <div class=A-1 style="border: 1px solid black; width: 30%; aspect-ratio: 1 / 1;"></div>
+
+        <p class="text-black text-xl">テスト</p>
+            
+                </div>
+        
+    </body>
+</html>
+
+    
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use App\Models\Book; //追加
 */
 
 //本：ダッシュボード表示(books.blade.php)
-Route::get('/', [BookController::class,'index'])->middleware(['auth'])->name('book_index');
-Route::get('/dashboard', [BookController::class,'index'])->middleware(['auth'])->name('dashboard');
+//Route::get('/', [BookController::class,'index'])->middleware(['auth'])->name('book_index');
+//Route::get('/dashboard', [BookController::class,'index'])->middleware(['auth'])->name('dashboard');
 
 //本：追加 
 Route::post('/books',[BookController::class,"store"])->name('book_store');
@@ -39,6 +39,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+Route::get('/', function () {
+    return view('guests');
+});
+
 });
 
 require __DIR__.'/auth.php';
