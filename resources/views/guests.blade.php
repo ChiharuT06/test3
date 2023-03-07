078b2316d7be4d70668cea93ea2bd2d94d5ccef7
ChiharuT06 <u.chiharu0909@gmail.com>
2023-03-07 09:23:46 +0000

    6th commit

--- resources/views/guests.blade.php
+++ resources/views/guests.blade.php
@@ -1,13 +1,93 @@
-<x-app-layout>
-
-            <div class=A-1 style="border: 1px solid black; width: 30%; aspect-ratio: 1 / 1;"></div>
-
-        <p class="text-black text-xl">テスト</p>
-            
-                </div>
-        <div class=""></div>
-        
-
-</x-app-layout>
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>座席レイアウト</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     
+</head>
+<x-app-layout>
+<body>
+
+ <h1>座席一覧</h1>
+  
+    
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 border-separate border border-solid border-2 border-indigo-600 ">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
+            <tr>
+                <th scope="col" class="px-6 py-3  border border-solid border-2 border-indigo-600 ">
+                    列
+                </th>
+                <th scope="col" class="px-6 py-3 " >
+                    
+                </th>
+                <th scope="col" class="px-6 py-3 ">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3 ">
+                    
+                </th>
+            </tr>
+        </thead>
+        
+            <tr class="bg-white  dark:bg-gray-800 ">
+                <th scope="row" class="px-6 py-4  text-2xl gray-900 whitespace-nowrap dark:text-white  border border-solid border-2 border-indigo-600 ">
+                    A
+                </th>
+                <td class= "px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                1<div id="A-1">[着席]</div>  
+                </td>
+                <td class="px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                <button>2</button>  
+                </td>
+                <td class="px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                <button>3</button>  
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 text-2xl gray-900 whitespace-nowrap dark:text-white border border-solid border-2 border-indigo-600">
+                    B
+                </th>
+                <td class="px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                <button>1</button>  
+                </td>
+                <td class="px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                <button>2</button>  
+                </td>
+                <td class="px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                <button>3</button>  
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 text-2xl gray-900 whitespace-nowrap dark:text-white border border-solid border-2 border-indigo-600">
+                    C
+                </th>
+                <td class="px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                <button>1</button>  
+                </td>
+                <td class="px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                <button>2</button>  
+                </td>
+                <td class="px-6 py-4 text-2xl border border-solid border-2 border-indigo-600">
+                <button>3</button>  
+                </td>
+            </tr>
+    
+    </table>
+</div>
+<script>
+   document.getElementById("A-1").onclick = function() {
+  document.getElementById("A-1").innerHTML = "着席中";
+};
+    
+   
+</script>
+    
+</body>
+
+</x-app-layout>    
+</html>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
+            
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
@@ -11,4 +12,6 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    
 });
+
