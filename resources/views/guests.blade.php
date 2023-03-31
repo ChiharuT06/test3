36dc95c96e5462d08e7c87a83952d3a461cb72c8
ChiharuT06 <u.chiharu0909@gmail.com>
2023-03-31 04:27:32 +0000

    18th commit

--- resources/views/guests.blade.php
+++ resources/views/guests.blade.php
@@ -204,12 +204,8 @@ if(data.id){
 
 var channel = pusher.subscribe("my-channel2"); //'my-channel'というチャンネルを作成している
 channel.bind("my-event2", function (data) {
-if(data.id){
-  document.querySelector("#" + data.seat_id).innerHTML = data.id;
-  document
-    .querySelector("#" + data.seat_id)
-    .classList.add("pointar-events-none");}
-    else { document.querySelector("#" + data.seat_id).classList.remove("pointar-events-none");
+
+      document.querySelector("#" + data.seat_id).classList.remove("pointar-events-none");
 
 　　  document.querySelector("#" + data.seat_id).textContent = "空席"
 
