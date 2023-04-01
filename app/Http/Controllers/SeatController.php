74d3e78108abcad2578368a7adabea27d07b1b3a
ChiharuT06 <u.chiharu0909@gmail.com>
2023-04-01 02:39:57 +0000

    21th commit

--- app/Http/Controllers/SeatController.php
+++ app/Http/Controllers/SeatController.php
@@ -17,7 +17,7 @@ class SeatController extends Controller
     }
      public function destroy(Request $request)//一つの画面を見る
     {      
-        event(new MyEvent2($request->null, $request->null));
+        event(new MyEvent2($request->id, $request->seatId));
            return response()->json(
         $request,JSON_UNESCAPED_UNICODE
     ); 
